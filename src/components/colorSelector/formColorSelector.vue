8bf3cb93bdea02602a9af81c974dc3020bd750a7
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-06-10 21:55:56 -0400

    <fixing> Color Tag Selector

---
+++ src/components/colorSelector/formColorSelector.vue
@@ -0,0 +1,87 @@
+<script setup>
+import {ref, defineEmits} from "vue";
+
+const emits = defineEmits(['onSelect'])
+const selectedColorValue = ref("")
+
+function sendSelectedColor() {
+  emits('onSelect', selectedColorValue.value)
+  console.log("Event Triggered")
+}
+</script>
+
+<template>
+  <a-select :value="selectedColorValue" @change="sendSelectedColor()">
+    <a-select-option value="red">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/red.svg">
+        </div>
+        <div>红色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="orange">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/orange.svg">
+        </div>
+        <div>橙色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="yellow">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/yellow.svg">
+        </div>
+        <div>黄色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="green">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/green.svg">
+        </div>
+        <div>绿色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="indigo">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/indigo.svg">
+        </div>
+        <div>青色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="blue">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/blue.svg">
+        </div>
+        <div>蓝色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="violet">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/violet.svg">
+        </div>
+        <div>紫色</div>
+      </div>
+    </a-select-option>
+    <a-select-option value="black">
+      <div class="row-display">
+        <div style="margin-left: 5px; margin-right: 5px">
+          <img src="@/assets/icons/colorTags/black.svg">
+        </div>
+        <div>黑色</div>
+      </div>
+    </a-select-option>
+  </a-select>
+</template>
+
+<style scoped>
+.row-display {
+  display: flex;
+  flex-direction: row;
+}
+</style>
--- src/views/tags/createTag.vue
+++ src/views/tags/createTag.vue
@@ -1,14 +1,37 @@
 <script setup>
-
 import HeaderContentView from "@/components/splitViews/headerContentView.vue";
+import {ref} from "vue";
+import FormColorSelector from "@/components/colorSelector/formColorSelector.vue";
+
+const selectableColors = ref([])
+
+selectableColors.value = [
+  {color: "red", tagIcon: "red.svg", displayName: "红"},
+  {color: "orange", tagIcon: "orange.svg", displayName: "橙"},
+  {color: "yellow", tagIcon: "yellow.svg", displayName: "黄"},
+  {color: "green", tagIcon: "green.svg", displayName: "绿"},
+  {color: "indigo", tagIcon: "indigo.svg", displayName: "青"},
+  {color: "blue", tagIcon: "blue.svg", displayName: "蓝"},
+  {color: "violet", tagIcon: "violet.svg", displayName: "紫"},
+  {color: "black", tagIcon: "black.svg", displayName: "黑"},
+]
 </script>
 
 <template>
 <HeaderContentView title="创建新的标签" sub-title="创建新标签以分类项目">
-  创建页面示例
+  标签名称
+  <a-input placeholder="标签名称"></a-input>
+  标签颜色
+  <form-color-selector></form-color-selector>
+  标签描述
+  <a-textarea></a-textarea>
 </HeaderContentView>
 </template>
 
 <style scoped>
-
+.row-display {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
 </style>
