84063990fe886377244c5fe8011713d6272c4272
kimsse <ilovemcmrcreeper@qq.com>
2023-07-05 19:29:07 +0800

    <Update> project structure update

--- src/config/config.js
+++ src/config/config.js
@@ -3,11 +3,11 @@ import unfs from "../lib/fs/basicFsHandler"
 
 let UnionProjectGlobalConfig = {
     language: "zh_cn",
-    createMethods: "./config/createMethods",
-    metadata: "./config/metadata",
+    createMethods: "../config/createMethods",
+    metadata: "../config/metadata",
     defaultMetadataLocation: path.join(__dirname, '/createMethods'),
-    publicResources: "./public",
-    settings: "./config/settings.json"
+    publicResources: "../public_resources",
+    settings: "../config/settings.json"
 }
 
 export let UnionProjectGlobalConfigData = UnionProjectGlobalConfig;
